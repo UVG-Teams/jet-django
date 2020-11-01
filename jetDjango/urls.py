ea84fb077c0cf5f56416100a80de95c8bdc90427
FR98 <ros18676@uvg.edu.gt>
2020-11-01 10:47:43 -0600

    Add views

--- jetDjango/urls.py
+++ jetDjango/urls.py
@@ -18,11 +18,11 @@ from django.conf.urls import url, include
 from django.urls import path
 
 from rest_framework import routers
-# from django_rest_framework_jet.views import (
-#     obtain_jet_token,
-#     refresh_jet_token,
-#     verify_jet_token,
-# )
+from rest_framework_jet.views import (
+    generate_jet,
+    verify_jet,
+    refresh_jet,
+)
 
 from init import create_initial_data
 
@@ -38,7 +38,7 @@ urlpatterns = [
     url(r'^admin/initial-data/', create_initial_data),
     url(r'^api-auth/', include('rest_framework.urls', namespace = 'rest_framework')),
     url(r'^api/', include(router.urls)),
-    # url(r'^api/token-auth/', obtain_jet_token),
-    # url(r'^api/token-refresh/', refresh_jet_token),
-    # url(r'^api/token-verify/', verify_jet_token),
+    url(r'^api/token-auth/', generate_jet),
+    url(r'^api/token-verify/', verify_jet),
+    url(r'^api/token-refresh/', refresh_jet),
 ]
--- rest_framework_jet/views.py
+++ rest_framework_jet/views.py
@@ -1,3 +1,35 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
-# Create your views here.
+from jetDjango.settings import GLOBAL_JET
+from jet.utils import hmac_sha256
+
+
+class GenerateJET(APIView):
+
+    def post(self, request, *args, **kwargs):
+        payload = {}
+        user_secret = hmac_sha256('user-password', 'user-password', 'ascii')
+        token = GLOBAL_JET.encrypt(user_secret, payload)
+        return Response(token)
+
+class VerifyJET(APIView):
+
+    def post(self, request, *args, **kwargs):
+        payload = {}
+        user_secret = hmac_sha256('user-password', 'user-password', 'ascii')
+        token = GLOBAL_JET.encrypt(user_secret, payload)
+        return Response(token)
+
+class RefreshJET(APIView):
+
+    def post(self, request, *args, **kwargs):
+        payload = {}
+        user_secret = hmac_sha256('user-password', 'user-password', 'ascii')
+        token = GLOBAL_JET.encrypt(user_secret, payload)
+        return Response(token)
+
+generate_jet = GenerateJET.as_view()
+verify_jet = VerifyJET.as_view()
+refresh_jet = RefreshJET.as_view()
