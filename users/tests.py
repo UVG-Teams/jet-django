1b6a793f4e90905d7bedf4b67833ada07634be4f
Marco <marcofuentes1607@gmail.com>
2020-11-18 23:56:26 -0600

    Token auth test

--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,23 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.contrib.auth.models import User
+from rest_framework.test import APIRequestFactory, APIClient
 
-# Create your tests here.
+class UserTestCase(TestCase):
+  def setUp(self):
+    self.credentials = {
+        'username': 'testUser',
+        'password': 'testpassword'}
+    User.objects.create_user(**self.credentials)
+    self.token = ''
+
+  def test_get_token(self):
+    c = self.client
+    response = c.post('/api/token-auth/' , self.credentials)
+    self.token = response.data['token']
+    self.assertTrue('token' in response.data)
+
+  def test_verify_token(self):
+    c = self.client
+    response = c.post('/api/token-verify/' , {'token': self.token})
+    print(response)
+    # self.assertTrue('token' in response.data)
