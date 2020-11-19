38cf922b9ac67b169f549402536a10e9622bae84
Marco <marcofuentes1607@gmail.com>
2020-11-19 00:18:34 -0600

    Test decrypt payload

--- users/tests.py
+++ users/tests.py
@@ -1,9 +1,11 @@
 from django.test import TestCase, Client
 from django.contrib.auth.models import User
 from rest_framework.test import APIRequestFactory, APIClient
+from django.conf import settings
 
 class UserTestCase(TestCase):
   def setUp(self):
+    self.JET = settings.GLOBAL_JET
     self.credentials = {
         'username': 'testUser',
         'password': 'testpassword'}
@@ -18,9 +20,30 @@ class UserTestCase(TestCase):
 
   def test_verify_token(self):
     c = self.client
+    response = c.post('/api/token-auth/' , self.credentials)
+    token = response.data['token']
     header = {
-      'Authorization': 'JWT {}'.format(self.token)
+      'Authorization': 'JET {}'.format(self.token)
     }
-    response = c.post('/api/token-verify/' ,data = {'token': self.token}, **header)
-    print(response.data)
-    # self.assertTrue('token' in response.data)
+    response = c.post('/api/token-verify/' ,data = {'token': token}, **header)
+    self.assertTrue(response.data['valid'])
+
+  def test_verify_token_jwt(self):
+    c = self.client
+    token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c'
+    header = {
+      'Authorization': 'JET {}'.format(token)
+    }
+    try:
+      response = c.post('/api/token-verify/' ,data = {'token': token}, **header) # Esto deberia lanzar un super error
+      self.assertEquals(response.status, 500)
+    except:
+      self.assertEquals(True, True) #Esto deberia de pasar
+
+
+  def test_decrypt_token(self):
+    c = self.client
+    response = c.post('/api/token-auth/' , self.credentials)
+    token = response.data['token']
+    meta , payload = self.JET.decrypt_from_PK(token)
+    self.assertTrue('id' in payload)
